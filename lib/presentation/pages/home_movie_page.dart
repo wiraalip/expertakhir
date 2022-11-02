bef9ac74b00a69727fff4493f02c2729422b9407
unknown <wiramuhammadalif@gmail.com>
2022-11-02 09:33:55 +0700

    firebase crashlytics and analytics

--- lib/presentation/pages/home_movie_page.dart
+++ lib/presentation/pages/home_movie_page.dart
@@ -7,6 +7,7 @@ import 'package:ditonton/presentation/pages/now_playing_movies.dart';
 import 'package:ditonton/presentation/pages/popular_movies_page.dart';
 import 'package:ditonton/presentation/pages/search_page.dart';
 import 'package:ditonton/presentation/pages/top_rated_movies_page.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
